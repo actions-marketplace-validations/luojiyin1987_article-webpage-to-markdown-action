931c8de8a8a164c0c4380d67012117ee010a2015
luojiyin <luojiyin@hotmail.com>
2022-03-17 11:12:43 +0800

    chore: when authorName not found, set url anoymous

--- dist/index.js
+++ dist/index.js
@@ -48955,7 +48955,7 @@ exports.HTMLtoMarkdown = (html) =>
     // Original author's signature
     const authorName = authorCardName.text() || 'Anonymous';
     // Original author's personal page
-    const authorURL = hostURL_EN + authorCardName.attr('href');
+    const authorURL = hostURL_EN + (authorCardName.attr('href') || 'Anonymous');
 
     if (!articleTitle || !authorName || (authorURL === hostURL_EN))
       return reject(Err_DOMWrong);
